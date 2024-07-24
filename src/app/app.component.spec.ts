ccf3ab01483bb76ea3b23bf5204dab35e3efb959
BD24K <bryanjosueflores24@gmail.com>
2024-07-24 12:42:17 -0600

    fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ladingpage-the-point' title`, () => {
+  it(`should have the 'thepoint' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ladingpage-the-point');
+    expect(app.title).toEqual('thepoint');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ladingpage-the-point');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, thepoint');
   });
 });
