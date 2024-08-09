cacead6a8aacd802584f2fd4846bac7ee40a045f
JosueFlores777 <bryanjosueflores24@gmail.com>
2024-08-09 13:12:56 -0600

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,54 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule,FooterComponent, SlickCarouselModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'thepoint';
+  slides = [
+    { img: 'https://images.pexels.com/photos/1229852/pexels-photo-1229852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { img: 'https://images.pexels.com/photos/1229852/pexels-photo-1229852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { img: 'https://images.pexels.com/photos/1229852/pexels-photo-1229852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { img: 'https://images.pexels.com/photos/1229852/pexels-photo-1229852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { img: 'https://images.pexels.com/photos/1229852/pexels-photo-1229852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { img: 'https://images.pexels.com/photos/1229852/pexels-photo-1229852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { img: 'https://images.pexels.com/photos/1229852/pexels-photo-1229852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
+    { img: 'https://images.pexels.com/photos/1229852/pexels-photo-1229852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' }
+  ];
+  slideConfig = { slidesToShow: 4, slidesToScroll: 4 };
+
+
+  removeSlide() {
+    if (this.slides.length > 0) {
+      this.slides.pop();
+    }
+  }
+
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
+
+  trackByFn(index: number, slide: any) {
+    return index;
+  }
 }
